8954ff649eaa7ef12580bd52f5687ea058c74457
Gniewosz Drwięga <95492178+gniewko-d@users.noreply.github.com>
2022-03-08 17:20:30 +0100

    gui + video

--- manual_labeler.py
+++ manual_labeler.py
@@ -9,9 +9,14 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 import easygui
 
-
-
+key_pressed = False
+frameTime = 50
 video_file = None
+start_frame = None
+start_frame_freezed = None
+stop_frame = None
+current_label = "test"
+
 label_1_name = f"{None}"
 label_2_name = f"{None}"
 label_3_name = f"{None}"
@@ -35,7 +40,7 @@ class Application:
         self.text = f"Current video: {None}"
         self.current_video = tk.Text(self.first_frame, height = 1, width = 20)
         self.current_video.insert(tk.INSERT, self.text)
-        self.desired_font = tk.font.Font( size = 10, weight = "bold")
+        self.desired_font = tk.font.Font(size = 10, weight = "bold")
         self.current_video.configure(font = self.desired_font)
         self.current_video.pack(side=tk.LEFT)
 
@@ -51,12 +56,12 @@ class Application:
         self.third_frame_v1 = tk.Frame(self.root)
         self.third_frame_v1.pack(side = tk.TOP)
         
-        self.start_labeling = tk.Button(self.third_frame_v1, text="Start labeling", command = self.start_vido)
+        self.start_labeling = tk.Button(self.third_frame_v1, text="Start labeling", command = start_vido1)
         self.start_labeling.pack(side=tk.LEFT)
         
     def easy_open(self):
         global video_file
-        video_file = easygui.fileopenbox(title="Select An Video", filetypes= ["*.gif", "*.flv", "*.avi", "*.amv", "*.mp4"])
+        video_file = easygui.fileopenbox(title="Select An Video", filetypes= [".gif", ".flv", ".avi", ".amv", "*.mp4"])
         if video_file != None:
             messagebox.showinfo("Information box", "Video uploaded")
             video_title = video_file.split("\\")
@@ -73,7 +78,7 @@ class Application:
         self.first_frame_v1.pack()
         self.instruction = tk.Text(self.first_frame_v1, height = 30, width = 70)
         self.text_v1 = "Press on your keyboard:\n a = move one frame backward\n d = move one frame forward\n q = escape from video and save markers\n p = pause the video\n r = restart the video (keep the markers applied)\n w = slow down video (have to be pressed constantly)\n e = frame to which (without it) all the preceding ones will\n\t be appropriately marked (depends on labels name set by user).\n\t Start point is set by key 1-9\n key 1-9 = label current frame and jumpt to next one or\n\t set the beginning of the range.\n\t Next you can move to whatever frame (backward or forward)\n\t and there set the end of the range by key e.\n\t All frames within that range will be labeled\n g = delete label of current frame\n h = removes the last labelled range\n"
-        conteiner = ["~"*70, "~"*70, self.text_v1, "="*70, "="*70]
+        conteiner = [""*70, ""*70, self.text_v1, "="*70, "="*70]
         conteiner_v1 = ["green", "grey", "red", "grey", "green"]
         for i in range(len(conteiner)):
             self.instruction.insert(tk.INSERT, conteiner[i])
@@ -183,68 +188,15 @@ class Application:
     def label_changer(self):
         global label_1_name, label_2_name, label_3_name, label_4_name, label_5_name, label_6_name, label_7_name, label_8_name, label_9_name
 
-        label_1_name = self.label_1_text_box.get("1.0","end")
-        label_2_name = self.label_2_text_box.get("1.0","end")
-        label_3_name = self.label_3_text_box.get("1.0","end")
-        label_4_name = self.label_4_text_box.get("1.0","end")
-        label_5_name = self.label_5_text_box.get("1.0","end")
-        label_6_name = self.label_6_text_box.get("1.0","end")
-        label_7_name = self.label_7_text_box.get("1.0","end")
-        label_8_name = self.label_8_text_box.get("1.0","end")
-        label_9_name = self.label_9_text_box.get("1.0","end")
-    
-    def start_vido(self):
-        global video_file, cap
-        title_window = "Mnimalistic Player"
-        cv2.namedWindow(title_window)
-        cv2.moveWindow(title_window,750,150)
-        cap = cv2.VideoCapture(video_file)
-        tots = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        cv2.createTrackbar('frame', title_window, 0,int(tots)-1, getFrame)
-        label = None
-        frameTime = 50
-        start_frame = None
-        stop_frame = None
-        start_frame_freezed = None
-        key_pressed = False
-        df = pd.DataFrame(columns = ["Label1"], index = range(1, int(tots) + 1))
-        delete_mode = np.nan
-        while(cap.isOpened()):
-            ret, frame = cap.read()
-            if ret == True:
-                cv2.imshow(title_window, frame)
-                current_frames = cap.get(cv2.CAP_PROP_POS_FRAMES)
-                cv2.setTrackbarPos('frame',title_window, int(current_frames))
-                if keyboard.is_pressed('a'):
-                    frame_changer(cap, "back", 1)
-                if keyboard.is_pressed('d'):
-                    frame_changer(cap, "front", 1)
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    cap.release()
-                    cv2.destroyAllWindows()
-                    break
-                if keyboard.is_pressed('p'):
-                    cv2.waitKey(-1) #wait until any key is pressed
-                if keyboard.is_pressed("r"):
-                    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                    cv2.setTrackbarPos('frame',title_window, 0)
-                    cv2.waitKey(-1)
-                if keyboard.is_pressed('w'):
-                    cv2.waitKey(frameTime)
-                if keyboard.is_pressed('e'):
-                    end_key(df, "test")
-                if keyboard.is_pressed('1'):
-                    step_mode(df, "test",cap)
-                if keyboard.is_pressed('g'):
-                    delete_mode(df, np.nan)
-                if keyboard.is_pressed('h'):
-                    ctrl_alt_delet(df)
-                if keyboard.is_pressed('g'):
-                    test = cap.get(cv2.CAP_PROP_POS_FRAMES)
-                    df.iloc[int(test), 0] = delete_mode
-                    cv2.waitKey(-1)
-                else:
-                    break
+        label_1_name = self.label_1_text_box.get("1.0", "end")
+        label_2_name = self.label_2_text_box.get("1.0", "end")
+        label_3_name = self.label_3_text_box.get("1.0", "end")
+        label_4_name = self.label_4_text_box.get("1.0", "end")
+        label_5_name = self.label_5_text_box.get("1.0", "end")
+        label_6_name = self.label_6_text_box.get("1.0", "end")
+        label_7_name = self.label_7_text_box.get("1.0", "end")
+        label_8_name = self.label_8_text_box.get("1.0", "end")
+        label_9_name = self.label_9_text_box.get("1.0", "end")
 
 
 def getFrame(frame_nr):
@@ -273,8 +225,9 @@ def frame_changer(video, direction, frame_num):
 
 
 def step_mode(data, label, video):
-    global key_pressed, start_frame
+    global key_pressed, start_frame, current_label
     if key_pressed:
+        current_label = label
         inital = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
         next_frame = inital + 1
         data.iloc[inital-2, 0] = label
@@ -295,6 +248,7 @@ def step_mode(data, label, video):
 def end_key(data, label):
     global start_frame, key_pressed, start_frame_freezed, stop_frame
     if key_pressed:
+        
         stop_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
         start_frame_freezed = start_frame
         if stop_frame >= start_frame:
@@ -320,75 +274,53 @@ def ctrl_alt_delet(data):
         data.iloc[stop_frame:start_frame_freezed, 0] = np.nan
         cv2.waitKey(-1)
 
-
-title_window = "Mnimalistic Player"
-cv2.namedWindow(title_window)
-cv2.moveWindow(title_window,750,150)
-cap = cv2.VideoCapture(video_file)
-tots = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-cv2.createTrackbar('frame', title_window, 0,int(tots)-1, getFrame)
-
-
-
-label = None
-frameTime = 50
-start_frame = None
-stop_frame = None
-start_frame_freezed = None
-key_pressed = False
-df = pd.DataFrame(columns = ["Label1"], index = range(1, int(tots) + 1))
-
-delete_mode = np.nan
-df = pd.DataFrame(columns = ["Label1"], index = range(1, int(tots) + 1))
-
-print("File exist:", os.path.exists(video_file))
-
-
-
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        cv2.imshow(title_window, frame)
-        current_frames = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        cv2.setTrackbarPos('frame',title_window, int(current_frames))
-        if keyboard.is_pressed('a'):
-            frame_changer(cap, "back", 1)
-        if keyboard.is_pressed('d'):
-             frame_changer(cap, "front", 1)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cap.release()
-            cv2.destroyAllWindows()
-            break
-        if keyboard.is_pressed('p'):
-            cv2.waitKey(-1) #wait until any key is pressed
-        if keyboard.is_pressed("r"):
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            cv2.setTrackbarPos('frame',title_window, 0)
-            cv2.waitKey(-1)
-        if keyboard.is_pressed('w'):
-           cv2.waitKey(frameTime)
-        if keyboard.is_pressed('e'):
-            end_key(df, "test")
-        if keyboard.is_pressed('1'):
-            step_mode(df, "test",cap)
-        if keyboard.is_pressed('g'):
-            delete_mode(df, np.nan)
-        if keyboard.is_pressed('h'):
-            ctrl_alt_delet(df)
-        if keyboard.is_pressed('g'):
-            test = cap.get(cv2.CAP_PROP_POS_FRAMES)
-            df.iloc[int(test), 0] = delete_mode
-            cv2.waitKey(-1)
-    else: 
-
-        break
-
-# When everything done, release 
-# the video capture object
-cap.release()
-
+xd = None
 # Closes all the frames
 cv2.destroyAllWindows()
 video_object = Application()
 video_object.root.mainloop()
+def start_vido1():
+    global label_1_name, xd, cap, title_window, frameTime, df
+    title_window = "Mnimalistic Player"
+    cv2.namedWindow(title_window)
+    cv2.moveWindow(title_window,750,150)
+    if video_file == None:
+        messagebox.showerror("Error box", "Upload the video first ")
+    else:
+        cap = cv2.VideoCapture(video_file)
+        tots = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        cv2.createTrackbar('frame', title_window, 0,int(tots)-1, getFrame)
+        df = pd.DataFrame(columns = [label_1_name], index = range(1, int(tots) + 1))
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                cv2.imshow(title_window, frame)
+                current_frames = cap.get(cv2.CAP_PROP_POS_FRAMES)
+                cv2.setTrackbarPos('frame',title_window, int(current_frames))
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+                    cap.release()
+                    cv2.destroyAllWindows()
+                if keyboard.is_pressed('a'):
+                    frame_changer(cap, "back", 1)
+                if keyboard.is_pressed('d'):
+                    frame_changer(cap, "front", 1)
+                if keyboard.is_pressed('p'):
+                    cv2.waitKey(-1) #wait until any key is pressed
+                if keyboard.is_pressed("r"):
+                    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                    cv2.setTrackbarPos('frame',title_window, 0)
+                    cv2.waitKey(-1)
+                if keyboard.is_pressed('w'):
+                    cv2.waitKey(frameTime)
+                if keyboard.is_pressed('e'):
+                    end_key(df, current_label)
+                if keyboard.is_pressed('1'):
+                    step_mode(df, label_1_name, cap)
+                if keyboard.is_pressed('g'):
+                    delete_mode(df, np.nan)
+                if keyboard.is_pressed('h'):
+                    ctrl_alt_delet(df)
+        xd = df
+        cap.release()
+        cv2.destroyAllWindows()
